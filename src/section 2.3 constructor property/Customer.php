854db584fc149879e2307ac2f991948d7733516b
onose <ogbidionose@gmail.com>
2026-01-11 20:06:37 +0100

    add: section 2.3 constructor property

---
+++ src/section 2.3 constructor property/Customer.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+class Customer {
+    private ?PaymentProfile $paymentProfile = null;
+
+    public function getPaymentProfile(): ?PaymentProfile{
+        return $this->paymentProfile;
+    }
+}
